7ec82aebf2d9fafe2326f1abdf24296f4173db54
Jakub Olejarczyk <jakubolejarczyk98@gmail.com>
2024-05-08 10:51:47 +0200

    create basic snake movement

--- snake.py
+++ snake.py
@@ -1,11 +1,15 @@
 import os
 import keyboard
+import time
+import threading
 
 map_width = 50
 map_height = 50
 
 player_x = 5
 player_y = 5
+
+direction = "w"
 
 def clean_console():
 	if os.name == "nt":
@@ -23,19 +27,32 @@ def display_map():
 				line += " "
 		print(line)
 
+def monitor_keyboard_events():
+	global direction
+	while True:
+		if keyboard.is_pressed("w"):
+			direction = "w"
+		if keyboard.is_pressed("s"):
+			direction = "s"
+		if keyboard.is_pressed("a"):
+			direction = "a"
+		if keyboard.is_pressed("d"):
+			direction = "d"
+
+keyboard_thread = threading.Thread(target=monitor_keyboard_events)
+keyboard_thread.daemon = True
+keyboard_thread.start()
+
 while True:
-	event = keyboard.read_event()
-	if event.event_type == keyboard.KEY_DOWN:
-		if event.name == "q":
-			break;
-		elif event.name == "w":
-			player_y -= 1
-		elif event.name == "s":
-			player_y += 1
-		elif event.name == "a":
-			player_x -= 1
-		elif event.name == "d":
-			player_x += 1
+	if direction == "w":
+		player_y -= 1
+	elif direction == "s":
+		player_y += 1
+	elif direction == "a":
+		player_x -= 1
+	elif direction == "d":
+		player_x += 1
+	
 	clean_console()
 	display_map()
-	
+	time.sleep(0.025)
