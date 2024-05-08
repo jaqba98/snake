1dfc6c35e797f5ba1d867d84c3b8a0d7b9053eb6
Jakub Olejarczyk <jakubolejarczyk98@gmail.com>
2024-05-08 10:24:10 +0200

    create basic player movement

--- snake.py
+++ snake.py
@@ -1,16 +1,41 @@
 import os
 import keyboard
 
+map_width = 50
+map_height = 50
+
+player_x = 5
+player_y = 5
+
 def clean_console():
 	if os.name == "nt":
 		os.system("cls")
 	else:
 		os.system("clear")
 
+def display_map():
+	for y in range(map_width):
+		line = ""
+		for x in range(map_height):
+			if x == player_x and y == player_y:
+				line += "P"
+			else:
+				line += " "
+		print(line)
+
 while True:
 	event = keyboard.read_event()
-	if event.event_type == keyboard.KEY_DOWN and event.name == "q":
-		break;
-	print(event.name)
+	if event.event_type == keyboard.KEY_DOWN:
+		if event.name == "q":
+			break;
+		elif event.name == "w":
+			player_y -= 1
+		elif event.name == "s":
+			player_y += 1
+		elif event.name == "a":
+			player_x -= 1
+		elif event.name == "d":
+			player_x += 1
 	clean_console()
+	display_map()
 	
