76dafb9de55ebac92724c95106e34d34a7956fb3
Jakub Olejarczyk <jakubolejarczyk98@gmail.com>
2024-05-19 09:33:19 +0200

    remove GameMap class

--- snake.py
+++ snake.py
@@ -52,7 +52,6 @@ map_width = int(input("Enter the map width: "))
 map_height = int(input("Enter the map height: "))
 
 g_player  = player.Player(10, 10, "$")
-g_map = level.GameMap(map_width, map_height)
 score = 0
 
 appleX = 0
@@ -63,14 +62,13 @@ while run == True:
 	
 	print(score)
 	if (appleX == 0 and appleY == 0) or g_player.x == appleX and g_player.y == appleY: 
-		appleX = random.randrange(1, g_map.width - 2)
-		appleY = random.randrange(1, g_map.height - 2) 
+		#appleX = random.randrange(1, g_map.width - 2)
+		#appleY = random.randrange(1, g_map.height - 2) 
 		score += 1	
-		g_player.add_tail(g_player.x, g_player.y)
+		#g_player.add_tail(g_player.x, g_player.y)
 		
 	g_player.move_player(direction, g_map)
 
-	g_map.draw_game_map(g_player, appleX, appleY)
 	print(len(g_player.tails))
 	time.sleep(0.1)
 
