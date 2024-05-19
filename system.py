dcf05fb159e3d5eb61983b0adddc1fd37e69eec0
Jakub Olejarczyk <jakubolejarczyk98@gmail.com>
2024-05-19 23:13:36 +0200

    remove unused method

--- system.py
+++ system.py
@@ -1,12 +1,8 @@
 import os
 
-def get_system_name():
-	if os.name == "posix": return "unix"
-	if os.name == "nt": return "widnows"
-	raise Exception("Not supported os.name = " + os.name)
-
 def clear_console_screen():
 	if os.name == "nt":
 		os.system("cls")
 	elif os.name == "posix":
 		os.system("clear")
+
